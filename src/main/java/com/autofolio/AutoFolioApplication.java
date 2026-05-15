99f24b92e166aa9d8f94c90e93ac4b5564ca8571
luizhenrique3651 <41950243+luizhenrique3651@users.noreply.github.com>
2026-05-15 05:34:33 -0400

    feat: adicionadas dependencias iniciais do projeto

---
+++ src/main/java/com/autofolio/AutoFolioApplication.java
@@ -0,0 +1,13 @@
+package com.autofolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoFolioApplication.class, args);
+    }
+
+}
